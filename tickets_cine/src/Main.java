1011504affe8982ad04cca8b3c8b641396fccae5
Maria del Mar <mariadelmarmosqueracaicedo@gmail.com>
2024-09-15 09:16:42 -0500

    Tickets de cine terminado

--- tickets_cine/src/Main.java
+++ tickets_cine/src/Main.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -91,6 +90,7 @@ public class Main {
     }
 }
 
+// Clases
 class Pelicula {
     String nombre;
     String[][] asientos;
